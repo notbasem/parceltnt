c7ace8a34f34d3e39228ab4721fa02b265603ae3
notbasem <bhamid@live.at>
2022-10-06 10:02:06 +0200

    Input vom Lektor eingebaut

--- src/main/java/at/fhtw/swen3/services/impl/WarehouseApiController.java
+++ src/main/java/at/fhtw/swen3/services/impl/WarehouseApiController.java
@@ -1,13 +1,25 @@
 package at.fhtw.swen3.services.impl;
 
 
+import at.fhtw.swen3.persistence.Hop;
+import at.fhtw.swen3.persistence.Warehouse;
+import at.fhtw.swen3.services.ApiUtil;
 import at.fhtw.swen3.services.WarehouseApi;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.Optional;
 import javax.annotation.Generated;
+import javax.validation.Valid;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-09-24T13:08:29.856611Z[Etc/UTC]")
 @Controller
@@ -25,4 +37,39 @@ public class WarehouseApiController implements WarehouseApi {
         return Optional.ofNullable(request);
     }
 
+    // Von Interface kopieren
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/warehouse/{code}",
+            produces = { "application/json" }
+    )
+    public ResponseEntity<Hop> getWarehouse(
+            @Parameter(name = "code", description = "", required = true) @PathVariable("code") String code
+    ) {
+        getRequest().ifPresent(request -> {
+            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"code\" : \"code\", \"locationName\" : \"locationName\", \"processingDelayMins\" : 0, \"hopType\" : \"hopType\", \"description\" : \"description\", \"locationCoordinates\" : { \"lon\" : 1.4658129805029452, \"lat\" : 6.027456183070403 } }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+        });
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+    }
+
+    //Hier vom Interface kopieren
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/warehouse",
+            produces = { "application/json" },
+            consumes = { "application/json" }
+    )
+    //auf public ändern
+    public ResponseEntity<Void> importWarehouses(
+            @Parameter(name = "Warehouse", description = "", required = true) @Valid @RequestBody Warehouse warehouse
+    ) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
