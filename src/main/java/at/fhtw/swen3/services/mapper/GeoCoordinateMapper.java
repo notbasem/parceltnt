74cead6913c26cfb9b3a97ff2af9221b17e91c04
notbasem <bhamid@live.at>
2022-10-17 18:39:42 +0200

    weitere mapper erstellt

---
+++ src/main/java/at/fhtw/swen3/services/mapper/GeoCoordinateMapper.java
@@ -0,0 +1,13 @@
+package at.fhtw.swen3.services.mapper;
+
+import at.fhtw.swen3.persistence.entity.GeoCoordinateEntity;
+import at.fhtw.swen3.services.dto.GeoCoordinate;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface GeoCoordinateMapper {
+    GeoCoordinateMapper INSTANCE = Mappers.getMapper(GeoCoordinateMapper.class);
+    GeoCoordinateEntity dtoToEntity(GeoCoordinate geoCoordinate);
+    GeoCoordinate entityToDto(GeoCoordinateEntity geoCoordinateEntity);
+}
---
+++ src/main/java/at/fhtw/swen3/services/mapper/RecipientMapper.java
@@ -0,0 +1,13 @@
+package at.fhtw.swen3.services.mapper;
+
+import at.fhtw.swen3.persistence.entity.RecipientEntity;
+import at.fhtw.swen3.services.dto.Recipient;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RecipientMapper {
+    RecipientMapper INSTANCE = Mappers.getMapper(RecipientMapper.class);
+    RecipientEntity dtoToEntity(Recipient recipient);
+    Recipient entityToDto(RecipientEntity recipientEntity);
+}
