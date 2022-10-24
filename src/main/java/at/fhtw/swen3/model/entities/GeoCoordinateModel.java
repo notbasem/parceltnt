fc49894107d545a715c39eb9da335d807c4e03b3
notbasem <bhamid@live.at>
2022-10-24 13:15:17 +0200

    quickfix

--- src/main/java/at/fhtw/swen3/model/entities/GeoCoordinateModel.java
+++ src/main/java/at/fhtw/swen3/model/entities/GeoCoordinateModel.java
@@ -2,6 +2,8 @@ package at.fhtw.swen3.model.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.springframework.data.geo.Point;
 
 import javax.persistence.*;
 
@@ -13,6 +15,10 @@ public class GeoCoordinateModel {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    //@Type(type="org.hibernate.spatial.GeometryType")
+    //private Point location;
+
     private Double lat;
     private Double lon;
 }
--- src/main/java/at/fhtw/swen3/model/repositories/ParcelRepository.java
+++ src/main/java/at/fhtw/swen3/model/repositories/ParcelRepository.java
@@ -1,6 +1,7 @@
 package at.fhtw.swen3.model.repositories;
 
 import at.fhtw.swen3.model.entities.ParcelModel;
+import at.fhtw.swen3.model.entities.RecipientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface ParcelRepository extends JpaRepository<ParcelModel, Long> {
     ParcelModel findByTrackingId(String trackingId);
+    List<ParcelModel> findBySender(RecipientModel sender);
 }
